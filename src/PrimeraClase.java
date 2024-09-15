53a1fd63eb18c6496af92c536db32b0953f134ba
anahi <luna.calderon@mi.unc.edu.ar>
2024-09-15 04:16:00 -0300

    creando mi primera app Java con Intellij

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -1,5 +1,5 @@
 public class PrimeraClase {
     public static void main(String[] args) {
-        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
+        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java :)!");
     }
 }
