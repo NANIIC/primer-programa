cecc90bd347203d9cb2eb15041848256a6166402
anahi <luna.calderon@mi.unc.edu.ar>
2024-09-15 04:50:18 -0300

    Trabajando con textos

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,13 @@ public class Main {
 
         double media = (8.2 + 6.0 + 9.0) / 3;
         System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja
+                La mejor película del fin del milenio
+                Fué lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
     }
 }
